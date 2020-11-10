c48f29bfd8867f6c325723a793b60b1d6b6a676a
AydinYasin <60688953+AydinYasin@users.noreply.github.com>
2020-11-10 18:29:25 +0100

    Push agecny project

---
+++ Desktop/CMS Development/aydin-agency/gatsby-config.js
@@ -0,0 +1,55 @@
+module.exports = {
+  siteMetadata: {
+    title: `Aydin Agency`,
+    description: `Artist Agency`,
+    author: `yasin`,
+  },
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `gatsby-starter-default`,
+        short_name: `starter`,
+        start_url: `/`,
+        background_color: `#663399`,
+        theme_color: `#663399`,
+        display: `minimal-ui`,
+        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+      },
+    },
+    {
+      resolve:"gatsby-source-graphql",
+      options:{
+        typeName:"WPGraphql",
+        fieldName:"wpcontent",
+        url:"http://artist-agency.codobi.be/graphql", // You can also use it Local url/graphql server has to run!
+      },
+    },
+    {
+
+      resolve: `gatsby-plugin-google-fonts`,
+      
+      options: {
+      
+      fonts: [`Roboto`, `Oswald`],
+      
+      display: "swap",
+      
+      },  
+      }, 
+      'gatsby-plugin-styled-components',
+    // this (optional) plugin enables Progressive Web App + Offline functionality
+    // To learn more, visit: https://gatsby.dev/offline
+    // `gatsby-plugin-offline`,
+  ],
+}

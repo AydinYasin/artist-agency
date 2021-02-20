e5d0456fd199766ee23d4016b74776797ab688c7
AydinYasin <60688953+AydinYasin@users.noreply.github.com>
2021-02-20 15:07:01 +0100

    Added cards to Home page with boostrap

---
+++ Desktop/Laxay/LaxayWeb/.cache/sync-requires.js
@@ -0,0 +1,12 @@
+
+
+// prefer default export if available
+const preferDefault = m => (m && m.default) || m
+
+
+exports.components = {
+  "component---cache-dev-404-page-js": (preferDefault(require("C:\\Users\\Aydin\\Desktop\\Laxay\\LaxayWeb\\.cache\\dev-404-page.js"))),
+  "component---src-pages-404-js": (preferDefault(require("C:\\Users\\Aydin\\Desktop\\Laxay\\LaxayWeb\\src\\pages\\404.js"))),
+  "component---src-pages-index-js": (preferDefault(require("C:\\Users\\Aydin\\Desktop\\Laxay\\LaxayWeb\\src\\pages\\index.js")))
+}
+
